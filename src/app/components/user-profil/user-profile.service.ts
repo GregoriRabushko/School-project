0f269862e44b0a4b24241918b829b65554269f97
Gregory <zak_zan@bk.ru>
2023-11-16 12:04:08 +0300

    feat: split the array into two sub-arrays in user-service

--- src/app/components/user-profil/user-profile.service.ts
+++ src/app/components/user-profil/user-profile.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@angular/core';
-import {ClassSchool, TableItemsService} from "src/app/components/items-to-school/table-with-item/table-items.service";
+import {Injectable} from '@angular/core';
+
+// import {ClassSchool, TableItemsService} from "src/app/components/items-to-school/table-with-item/table-items.service";
 
 @Injectable({
   providedIn: 'root'
@@ -7,79 +8,287 @@ import {ClassSchool, TableItemsService} from "src/app/components/items-to-school
 export class UserProfileService {
 
 
-  scheduleSchool = [
+  scheduleWeek: DairyWeek[] = [
     {
-      month: 'Январь',
+      dayWeek: 'Понедельник',
+      dayMonth: '12',
       schedule: [
         {
-          day: '1',
-          grade: '',
-          lessonTopic: '',
-          homework: ''
+          nameSubject: 'Математика',
+          homework: '№3',
+          grade: '7'
         },
         {
-          day: '2',
-          grade: '',
-          lessonTopic: '',
-          homework: ''
+          nameSubject: 'Английский',
+          homework: 'Выучить слова',
+          grade: '6'
         },
         {
-          day: '3',
-          grade: '',
-          lessonTopic: '',
-          homework: ''
+          nameSubject: 'Русский',
+          homework: 'Написать сочинение',
+          grade: ''
         },
         {
-          day: '4',
-          grade: '',
-          lessonTopic: '',
-          homework: ''
+          nameSubject: 'Астрономия',
+          homework: 'Подготовиться к КР',
+          grade: ''
         },
         {
-          day: '5',
-          grade: '',
-          lessonTopic: '',
-          homework: ''
+          nameSubject: 'Физика',
+          homework: 'прочит. следю параграф ',
+          grade: '8'
         },
         {
-          day: '6',
-          grade: '',
-          lessonTopic: '',
-          homework: ''
+          nameSubject: 'ДПЮ',
+          homework: '',
+          grade: ''
+        },
+        {
+          nameSubject: 'ЧЗС',
+          homework: '',
+          grade: ''
+        },
+      ]
+    },
+    {
+      dayWeek: 'Вторник',
+      dayMonth: '13',
+      schedule: [
+        {
+          nameSubject: 'Математика',
+          homework: '',
+          grade: ''
+        },
+        {
+          nameSubject: 'Английский',
+          homework: '',
+          grade: ''
+        },
+        {
+          nameSubject: 'Русский',
+          homework: '',
+          grade: ''
+        },
+        {
+          nameSubject: 'Астрономия',
+          homework: '',
+          grade: ''
+        },
+        {
+          nameSubject: 'Физика',
+          homework: '',
+          grade: ''
+        },
+        {
+          nameSubject: 'ДПЮ',
+          homework: '',
+          grade: ''
+        },
+        {
+          nameSubject: 'ЧЗС',
+          homework: '',
+          grade: ''
+        },
+      ]
+    },
+    {
+      dayWeek: 'Среда',
+      dayMonth: '14',
+      schedule: [
+        {
+          nameSubject: 'Математика',
+          homework: '',
+          grade: ''
+        },
+        {
+          nameSubject: 'Английский',
+          homework: '',
+          grade: ''
+        },
+        {
+          nameSubject: 'Русский',
+          homework: '',
+          grade: ''
+        },
+        {
+          nameSubject: 'Астрономия',
+          homework: 'найти публикацию в библиотеке',
+          grade: ''
+        },
+        {
+          nameSubject: 'Физика',
+          homework: '',
+          grade: ''
+        },
+        {
+          nameSubject: 'ДПЮ',
+          homework: '',
+          grade: ''
+        },
+        {
+          nameSubject: 'ЧЗС',
+          homework: '',
+          grade: ''
+        },
+      ]
+    },
+    {
+      dayWeek: 'Четверг',
+      dayMonth: '15',
+      schedule: [
+        {
+          nameSubject: 'Математика',
+          homework: '',
+          grade: ''
+        },
+        {
+          nameSubject: 'Английский',
+          homework: '',
+          grade: ''
+        },
+        {
+          nameSubject: 'Русский',
+          homework: '',
+          grade: ''
+        },
+        {
+          nameSubject: 'Астрономия',
+          homework: '',
+          grade: ''
+        },
+        {
+          nameSubject: 'Физика',
+          homework: '',
+          grade: ''
+        },
+        {
+          nameSubject: 'ДПЮ',
+          homework: '',
+          grade: ''
+        },
+        {
+          nameSubject: 'ЧЗС',
+          homework: '',
+          grade: ''
+        },
+      ]
+    },
+    {
+      dayWeek: 'Пятница',
+      dayMonth: '16',
+      schedule: [
+        {
+          nameSubject: 'Математика',
+          homework: 'з.3',
+          grade: '7'
+        },
+        {
+          nameSubject: 'Английский',
+          homework: 'прочитать, перевести',
+          grade: '9'
+        },
+        {
+          nameSubject: 'Русский',
+          homework: 'дописать',
+          grade: ''
+        },
+        {
+          nameSubject: 'Астрономия',
+          homework: '',
+          grade: ''
+        },
+        {
+          nameSubject: 'Физика',
+          homework: '',
+          grade: ''
+        },
+        {
+          nameSubject: 'ДПЮ',
+          homework: '',
+          grade: ''
+        },
+        {
+          nameSubject: 'ЧЗС',
+          homework: '',
+          grade: ''
+        },
+      ]
+    },
+    {
+      dayWeek: 'Суббота',
+      dayMonth: '17',
+      schedule: [
+        {
+          nameSubject: 'Математика',
+          homework: '',
+          grade: ''
+        },
+        {
+          nameSubject: 'Английский',
+          homework: '',
+          grade: ''
+        },
+        {
+          nameSubject: 'Русский',
+          homework: '',
+          grade: ''
+        },
+        {
+          nameSubject: 'Астрономия',
+          homework: '',
+          grade: ''
+        },
+        {
+          nameSubject: 'Физика',
+          homework: '',
+          grade: ''
+        },
+        {
+          nameSubject: 'ДПЮ',
+          homework: '',
+          grade: ''
+        },
+        {
+          nameSubject: 'ЧЗС',
+          homework: '',
+          grade: ''
         },
       ]
     },
   ];
 
+  firstHalfOfTheWeek: DairyWeek[] = [];
+  secondHalfOfTheWeek: DairyWeek[] = [];
+
   constructor(
-    private readonly _tableItemsService:TableItemsService,
-  ) { }
-
-  weekSchedule:ClassSchool[] = this._tableItemsService.classesSchoolInfo;
-
-  getScheduleWeek(classNameStudent:string) {
-    let weekScheduleWithIdStudent:ClassSchool = {
-      id: '',
-      schedule: {
-        'Пн': [{room: '', nameLesson: ''}],
-        'Вт': [{room: '', nameLesson: ''}],
-        'Ср': [{room: '', nameLesson: ''}],
-        'Чт': [{room: '', nameLesson: ''}],
-        'Пт': [{room: '', nameLesson: ''}],
-        'Сб': [{room: '', nameLesson: ''}]
-      }
-    }
-    this.weekSchedule.forEach(item => {
-      if(item.id === classNameStudent) {
-       weekScheduleWithIdStudent = item;
-       console.log(weekScheduleWithIdStudent);
-      } else {
-        console.log(weekScheduleWithIdStudent);
-        console.log(classNameStudent);
-        console.log('Err: Данные не загрузились');
-      }
-    })
-    return weekScheduleWithIdStudent;
+    // private readonly _tableItemsService:TableItemsService,
+  ) {
   }
 
+
+  getScheduleWeek() {
+    // todo перенести эту функцию  на сервер, ну или оставить ее здесь
+    if (this.scheduleWeek.length > 0 && this.scheduleWeek.length === 6) {
+      this.scheduleWeek.forEach((item, index) => {
+        if (index < 3) {
+          this.firstHalfOfTheWeek.push(item);
+        } else {
+          this.secondHalfOfTheWeek.push(item);
+        }
+      })
+    } else {
+      console.log('Error: Not found!!!');
+    }
+    return [this.firstHalfOfTheWeek, this.secondHalfOfTheWeek];
+  }
+}
+
+export interface DairyWeek {
+  dayWeek: string,
+  dayMonth: string,
+  schedule: {
+    nameSubject: string,
+    homework: string,
+    grade: string
+  }[]
 }
